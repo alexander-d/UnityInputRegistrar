4cae3b9432500b847fcc5f8f71bbf2f419f920ac
alexander-d <dudledok@gmail.com>
2017-04-18 23:13:12 +0100

    Add files via upload

---
+++ ExampleUse/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using InputRegistrar;
+
+public class Player : MonoBehaviour
+{
+	[Range(1, 4)]
+	[SerializeField]
+	private int m_controllerNumber;
+
+	private InputManager m_inputManager;
+
+	private void Start()
+	{
+		m_inputManager = new InputManager<PS4InputRegistrar, int>(m_controllerNumber);
+
+		m_inputManager.RegisterButtonInput(new ButtonGesture(InputValue.Alpha, ButtonAction.OnPressDown), Foo);
+	}
+
+	private void Foo()
+	{
+		Debug.Log("Bar");
+	}
+
+	private void Update()
+	{
+		m_inputManager.Update();
+	}
+}
