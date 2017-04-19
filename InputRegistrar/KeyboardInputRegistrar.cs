da04bfd216a5cda761edd2ab079aba533301d05d
Donal <maccard@tcd.ie>
2017-04-19 12:21:23 +0100

    Added a helper method to register a button to common gestures, and an example of how to use it in Keyboard

--- InputRegistrar/KeyboardInputRegistrar.cs
+++ InputRegistrar/KeyboardInputRegistrar.cs
@@ -14,34 +14,16 @@ namespace InputRegistrar
 		{
 			base.Initialise();
 
-			RegisterButton(new ButtonGesture(InputValue.Alpha, ButtonAction.OnPressDown), KeyCode.Space);
-			RegisterButton(new ButtonGesture(InputValue.Alpha, ButtonAction.OnPress), KeyCode.Space);
-			RegisterButton(new ButtonGesture(InputValue.Alpha, ButtonAction.OnPressUp), KeyCode.Space);
-			RegisterButton(new ButtonGesture(InputValue.Beta, ButtonAction.OnPressDown), KeyCode.LeftControl);
-			RegisterButton(new ButtonGesture(InputValue.Beta, ButtonAction.OnPress), KeyCode.LeftControl);
-			RegisterButton(new ButtonGesture(InputValue.Beta, ButtonAction.OnPressUp), KeyCode.LeftControl);
-			RegisterButton(new ButtonGesture(InputValue.Gamma, ButtonAction.OnPressDown), KeyCode.RightControl);
-			RegisterButton(new ButtonGesture(InputValue.Gamma, ButtonAction.OnPress), KeyCode.RightControl);
-			RegisterButton(new ButtonGesture(InputValue.Gamma, ButtonAction.OnPressUp), KeyCode.RightControl);
-			RegisterButton(new ButtonGesture(InputValue.Delta, ButtonAction.OnPressDown), KeyCode.LeftAlt);
-			RegisterButton(new ButtonGesture(InputValue.Delta, ButtonAction.OnPress), KeyCode.LeftAlt);
-			RegisterButton(new ButtonGesture(InputValue.Delta, ButtonAction.OnPressUp), KeyCode.LeftAlt);
-			RegisterButton(new ButtonGesture(InputValue.Epsilon, ButtonAction.OnPressDown), KeyCode.RightAlt);
-			RegisterButton(new ButtonGesture(InputValue.Epsilon, ButtonAction.OnPress), KeyCode.RightAlt);
-			RegisterButton(new ButtonGesture(InputValue.Epsilon, ButtonAction.OnPressUp), KeyCode.RightAlt);
+			RegisterButton(InputValue.Alpha, KeyCode.Space);
+			RegisterButton(InputValue.Beta, KeyCode.LeftControl);
+			RegisterButton(InputValue.Gamma, KeyCode.RightControl);
+			RegisterButton(InputValue.Delta, KeyCode.LeftAlt);
+			RegisterButton(InputValue.Epsilon, KeyCode.RightAlt);
 
-			RegisterButton(new ButtonGesture(InputValue.Upsilon, ButtonAction.OnPressDown), KeyCode.UpArrow);
-			RegisterButton(new ButtonGesture(InputValue.Upsilon, ButtonAction.OnPress), KeyCode.UpArrow);
-			RegisterButton(new ButtonGesture(InputValue.Upsilon, ButtonAction.OnPressUp), KeyCode.UpArrow);
-			RegisterButton(new ButtonGesture(InputValue.Phi, ButtonAction.OnPressDown), KeyCode.LeftArrow);
-			RegisterButton(new ButtonGesture(InputValue.Phi, ButtonAction.OnPress), KeyCode.LeftArrow);
-			RegisterButton(new ButtonGesture(InputValue.Phi, ButtonAction.OnPressUp), KeyCode.LeftArrow);
-			RegisterButton(new ButtonGesture(InputValue.Chi, ButtonAction.OnPressDown), KeyCode.RightArrow);
-			RegisterButton(new ButtonGesture(InputValue.Chi, ButtonAction.OnPress), KeyCode.RightArrow);
-			RegisterButton(new ButtonGesture(InputValue.Chi, ButtonAction.OnPressUp), KeyCode.RightArrow);
-			RegisterButton(new ButtonGesture(InputValue.Psi, ButtonAction.OnPressDown), KeyCode.DownArrow);
-			RegisterButton(new ButtonGesture(InputValue.Psi, ButtonAction.OnPress), KeyCode.DownArrow);
-			RegisterButton(new ButtonGesture(InputValue.Psi, ButtonAction.OnPressUp), KeyCode.DownArrow);
+			RegisterButton(InputValue.Upsilon, KeyCode.UpArrow);
+			RegisterButton(InputValue.Phi, KeyCode.LeftArrow);
+			RegisterButton(InputValue.Chi, KeyCode.RightArrow);
+			RegisterButton(InputValue.Psi, KeyCode.DownArrow);
 		}
 
 		protected override void SwitchButtonAction(KeyValuePair<ButtonGesture, KeyCode> binding)
